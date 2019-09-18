7cc4700b71b304f552cf61602b790b2ce6091cf5
Rajnish Kumar <rajnish.kumar952@gmail.com>
2019-09-18 08:19:11 +0530

    Maximum absolute difference

---
+++ MaximumAbsoluteDiff.cpp
@@ -0,0 +1,26 @@
+/*
+Maximum Absolute Difference
+You are given an array of N integers, A1, A2 ,..., AN. Return maximum value of f(i, j) for all 1 ≤ i, j ≤ N. f(i, j) is defined as |A[i] - A[j]| + |i - j|, where |x| denotes absolute value of x. For example,
+A=[1, 3, -1]
+
+f(1, 1) = f(2, 2) = f(3, 3) = 0
+f(1, 2) = f(2, 1) = |1 - 3| + |1 - 2| = 3
+f(1, 3) = f(3, 1) = |1 - (-1)| + |1 - 3| = 4
+f(2, 3) = f(3, 2) = |3 - (-1)| + |2 - 3| = 5
+
+So, we return 5.
+*/
+
+int Solution::maxArr(vector<int> &A) {
+    
+    vector<int> X, Y;
+    for(int i=0;i<A.size();i++){
+        int xval=A[i] + i;
+        X.push_back(xval);
+        int yval=A[i]-i;
+        Y.push_back(yval);
+    }
+    sort(X.begin(), X.end());
+    sort(Y.begin(), Y.end());
+    return max((X[X.size()-1]-X[0]), (Y[Y.size()-1]-Y[0]));
+}
