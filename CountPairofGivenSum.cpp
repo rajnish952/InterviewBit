b09b023c4d88ae5773d8e99750cc0866d12d83cc
Rajnish Kumar <rajnish.kumar952@gmail.com>
2019-10-21 23:18:21 +0530

    Count pair which sums upto k

---
+++ CountPairofGivenSum.cpp
@@ -0,0 +1,43 @@
+/*
+Count of pairs with the given sum
+Given a sorted array of distinct integers A and an integer B, find and return how many pair of integers ( A[i], A[j] ) such that i != j have sum equal to B. 
+Input Format
+The first argument given is the integer array A.
+The second argument given is integer B.
+Output Format
+Return the number of pairs for which sum is equal to B.
+Constraints
+1 <= length of the array <= 100000
+1 <= A[i] <= 10^9 
+1 <= B <= 10^9
+For Example
+Input 1:
+    A = [1, 2, 3, 4, 5]
+    B = 5
+Output 1:
+    2
+
+Input 2:
+    A = [5, 10, 20, 100, 105]
+    B = 110
+Output 2:
+    2
+    */
+
+int Solution::solve(vector<int> &A, int B) {
+    int i=0, j=A.size()-1;
+    int count=0;
+    while(i<j){
+        if(A[i]+A[j]>B) //if sum is greater reduce j
+            j--;
+        else if(A[i]+A[j]<B) // sum is smaller increment i
+            i++;
+        else
+        {
+            count++; // sum equla either reduce j or increment i
+            j--;
+        }
+    }
+    
+    return count;
+}
